3a7c61a8b440d434d3c56b8e43c4ea66f24c493d
Lu Lu <apollu07@gmail.com>
2015-08-24 21:10:30 -0700

    First commit to Dota2InTheBag iOS app. Let's do it! -Lu Lu

--- Dota2InTheBag/ViewController.swift
+++ Dota2InTheBag/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        println("First commit to Dota2InTheBag iOS app. Let's do it! -Lu Lu")
     }
 
     override func didReceiveMemoryWarning() {
