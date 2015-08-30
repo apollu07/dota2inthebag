47fc04f64c3202a8c8f02dac8ffc375da149f030
Lu Lu <apollu07@gmail.com>
2015-08-29 19:26:47 -0700

    Add "add hero" button background and store which hero button is touched.

--- Dota2InTheBag/LineupViewController.swift
+++ Dota2InTheBag/LineupViewController.swift
@@ -11,6 +11,17 @@ import UIKit
 class LineupViewController: UIViewController {
     
     var matchLevel: MatchLevelViewController.MatchLevel?
+    
+    // Which hero button in the screen is touched.
+    // If no hero is selected for that position, we go to the hero selection screen.
+    // Otherwise we enable the user to edit/remove it.
+    // Allies  Enemies
+    // 0       5
+    // 1       6
+    // 2       7
+    // 3       8
+    // 4       9
+    var touchedHeroButtonID: Int?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,7 +35,38 @@ class LineupViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    // Detect hero button touch event
+    @IBAction func heroButton0Touched(sender: AnyObject) {
+        touchedHeroButtonID = 0
+    }
+    @IBAction func heroButton1Touched(sender: AnyObject) {
+        touchedHeroButtonID = 1
+    }
+    @IBAction func heroButton2Touched(sender: AnyObject) {
+        touchedHeroButtonID = 2
+    }
+    @IBAction func heroButton3Touched(sender: AnyObject) {
+        touchedHeroButtonID = 3
+    }
+    @IBAction func heroButton4Touched(sender: AnyObject) {
+        touchedHeroButtonID = 4
+    }
+    @IBAction func heroButton5Touched(sender: AnyObject) {
+        touchedHeroButtonID = 5
+    }
+    @IBAction func heroButton6Touched(sender: AnyObject) {
+        touchedHeroButtonID = 6
+    }
+    @IBAction func heroButton7Touched(sender: AnyObject) {
+        touchedHeroButtonID = 7
+    }
+    @IBAction func heroButton8Touched(sender: AnyObject) {
+        touchedHeroButtonID = 8
+    }
+    @IBAction func heroButton9Touched(sender: AnyObject) {
+        touchedHeroButtonID = 9
+    }
+    
     /*
     // MARK: - Navigation
 
